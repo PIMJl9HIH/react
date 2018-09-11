dd2ebb03d4b2065f24af1a61db56d50a5f73e60d
PIMJl9HIH <romjke2015.1993@gmail.com>
2018-09-11 13:06:41 +0300

    add task

--- src/components/CreateCategory.jsx
+++ src/components/CreateCategory.jsx
@@ -1,34 +1,35 @@
 import React from 'react';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { ListGroup, ListGroupItem } from 'reactstrap';
+import {ListGroup, ListGroupItem} from 'reactstrap';
 import '../App.css';
 
-const Category = ({ 
-		posts,
-		curPost,
-		onDismissCategory,
-    missingCategory,
-    addCategory,
-    toggleFunc
-	}) => 
-	<div >
-		<div className="post__category-container">
-			<div className="post__category-title">
-				<div >Post categories : </div>
+const Category = ({
+                    posts,
+                    curPost,
+                    onDismissCategory,
+                    missingCategory,
+                    addCategory,
+                    toggleFunc
+                  }) =>
+  <div>
+    <div className="post__category-container">
+      <div className="post__category-title">
+        <div>Post categories :</div>
         <div
           className="addCategory add button"
           onClick={(event) => toggleFunc(curPost, posts, event)}>
         </div>
-			</div>
-			{posts[curPost].category.map((item, index, arr) =>
-				 <div className="categoryItem" key={index}>
-					 {item}
-					 <div className="delete" onClick={() => onDismissCategory(item, index, arr, curPost, posts)}> </div>
-				 </div>
-			)}
+      </div>
+      {posts[curPost].category.map((item, index, arr) =>
+        <div className="categoryItem" key={index}>
+          {item}
+          <div className="delete" onClick={() => onDismissCategory(item, index, arr, curPost, posts)}></div>
+        </div>
+      )}
 
-      <div className="postAddingCategory category__list" style={{display: posts[curPost].visibilityAddingCategory ? 'block' : 'none'}}>
+      <div className="postAddingCategory category__list"
+           style={{display: posts[curPost].visibilityAddingCategory ? 'block' : 'none'}}>
         {/*<FormGroup>*/}
         {/*<Input  type="select" multiple  name="curPostCategory" onClick={(event) => addCategory(curPost, posts, event)}>*/}
         {/*<option disabled>Select</option>*/}
@@ -40,17 +41,16 @@ const Category = ({
 
         <ListGroup>
           {missingCategory(curPost).map((item, index) =>
-            <ListGroupItem className="categoryItem" tag="a" href="#" action key={index} onClick={(event) => addCategory(curPost, posts, event)} >{item} </ListGroupItem>
+            <ListGroupItem className="categoryItem" tag="a" href="#" action key={index}
+                           onClick={(event) => addCategory(curPost, posts, event)}>{item} </ListGroupItem>
           )}
         </ListGroup>
       </div>
 
-		</div>
+    </div>
 
 
-
-
-	</div>
+  </div>
 
 
 export default Category
